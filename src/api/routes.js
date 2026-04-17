7e3e9a6e80b64ff8f70adc558a39f1dc48e1d454
Angga0512 <mairlangga686@gmail.com>
2026-04-17 09:31:50 +0700

    implementasi jwt authentication and authorization

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
+const auth = require('./components/auth/auth-route');
 const books = require('./components/books/books-route');
 const users = require('./components/users/users-route');
 
 module.exports = () => {
   const app = express.Router();
 
+  auth(app);
   books(app);
   users(app);
 
